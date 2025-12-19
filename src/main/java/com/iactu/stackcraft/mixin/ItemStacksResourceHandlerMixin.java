e1bd1cd31a363fe1373597871916889d19bc7bc3
John <john@jbdm.org>
2025-12-18 22:09:36 -0600

    Ported to Minecraft 1.21.11

--- src/main/java/com/iactu/stackcraft/mixin/ItemStacksResourceHandlerMixin.java
+++ src/main/java/com/iactu/stackcraft/mixin/ItemStacksResourceHandlerMixin.java
@@ -2,15 +2,15 @@ package com.iactu.stackcraft.mixin;
 
 import com.iactu.stackcraft.StackcraftMod;
 import net.minecraft.world.item.Item;
-import net.neoforged.neoforge.transfer.item.ItemAccessItemHandler;
+import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(ItemAccessItemHandler.class)
-public abstract class ComponentItemHandlerMixin {
+@Mixin(ItemStacksResourceHandler.class)
+public abstract class ItemStacksResourceHandlerMixin {
     @ModifyConstant(
-            method = "Lnet/neoforged/neoforge/transfer/item/ItemAccessItemHandler;getCapacity(ILnet/neoforged/neoforge/transfer/item/ItemResource;)I",
+            method = "Lnet/neoforged/neoforge/transfer/item/ItemStacksResourceHandler;getCapacity(ILnet/neoforged/neoforge/transfer/item/ItemResource;)I",
             constant = @Constant(intValue = Item.ABSOLUTE_MAX_STACK_SIZE)
     )
     private int maximizeStackSize() {
